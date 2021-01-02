68d4fdb4f3d241dac7ff08f02ba4094c4216bb6c
June14th <38001464+June14th@users.noreply.github.com>
2021-01-02 12:41:18 +0800

    滑动窗口最大值
    滑动窗口，最大堆

---
+++ pkg239/Solution.java
@@ -0,0 +1,36 @@
+package pkg239;
+/**
+ * 给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+ *
+ * 返回滑动窗口中的最大值。
+ */
+import java.util.PriorityQueue;
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = {1,3,-1,-3,5,3,6,7};
+        int k = 3;
+        int[] res = maxSlidingWindow(nums,k);
+        for(int i:res){
+            System.out.print(i+", ");
+        }
+    }
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((i1,i2)-> {
+            return i1[0]==i2[0]?i2[1]-i1[1]:i2[0]-i1[0];
+        });
+        for (int i = 0; i < k; ++i) {
+            pq.offer(new int[]{nums[i], i});
+        }
+        int[] ans = new int[n - k + 1];
+        ans[0] = pq.peek()[0];
+        for (int i = k; i < n; ++i) {
+            pq.offer(new int[]{nums[i], i});
+            while (pq.peek()[1] <= i - k) {
+                pq.poll();
+            }
+            ans[i - k + 1] = pq.peek()[0];
+        }
+        return ans;
+    }
+}
