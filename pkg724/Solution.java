3664a95afa22aefe66dffff133cf7b2e5659bce3
June14th <38001464+June14th@users.noreply.github.com>
2021-01-28 09:01:37 +0800

    寻找数组的中心索引
    先求总和，再两倍之前的和加当前，看是否相等

---
+++ pkg724/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+        int total = Arrays.stream(nums).sum();
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            if(2*sum + nums[i] == total){
+                return i;
+            }
+            sum += nums[i];
+        }
+        return -1;
+    }
+}
